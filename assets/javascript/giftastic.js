5430c49481c627cf312d369504114f490f2e9f91
Dom <61214047+dominiquedawez@users.noreply.github.com>
2020-05-15 14:33:57 -0400

    Changed javascript to be able to take the element at the last index of the array Animals

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -1,1 +1,5 @@
-var animals = ["bear", "cat", "duck", "tiger","elephant", "rabbit", "wolf", "hourse"]
+// var animals = ["bear", "cat", "duck", "tiger","elephant", "rabbit", "wolf", "hourse"]
+
+// // get animals variable in a tag matched with a button and append buttons index to html
+
+// document.getElementById(animals);
